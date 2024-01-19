5fe3b810de3cfa34cd106415ccd3fb54ed885b51
Welisson Oliveira <welisson.oliveiraa@gmail.com>
2024-01-18 22:46:49 -0300

    cria o bean para que a injeção de dependencia dos parses funcione

---
+++ src/main/java/br/com/stoom/store/config/ApplicationConfig.java
@@ -0,0 +1,36 @@
+package br.com.stoom.store.config;
+
+import br.com.stoom.store.controller.dto.BrandDTO;
+import br.com.stoom.store.controller.dto.CategoryDTO;
+import br.com.stoom.store.controller.dto.ProductDTO;
+import br.com.stoom.store.controller.parsers.Parser;
+import br.com.stoom.store.model.Brand;
+import br.com.stoom.store.model.Category;
+import br.com.stoom.store.model.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    @Bean
+    public Map<Class, Parser> getParser(
+            @Qualifier("productParser") final Parser<Product, ProductDTO> productParser,
+            @Qualifier("categoryParser") final Parser<Category, CategoryDTO> categoryParser,
+            @Qualifier("brandParser") final Parser<Brand, BrandDTO> brandParser
+    ) {
+        final Map<Class, Parser> parsers = new HashMap<>();
+
+        parsers.put(Product.class, productParser);
+        parsers.put(Category.class, categoryParser);
+        parsers.put(Brand.class, brandParser);
+
+        return parsers;
+    }
+}
