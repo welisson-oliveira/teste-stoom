6250be80485f9fc8bc29306f7de6446686c216d1
Welisson Oliveira <welisson.oliveiraa@gmail.com>
2024-01-20 17:49:05 -0300

    adiciona um exception handler para violação de dados

--- src/main/java/br/com/stoom/store/exceptions/DefaultExceptionHandler.java
+++ src/main/java/br/com/stoom/store/exceptions/DefaultExceptionHandler.java
@@ -1,13 +1,20 @@
 package br.com.stoom.store.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class DefaultExceptionHandler {
+
+    @ExceptionHandler({DataIntegrityViolationException.class, HttpMessageNotReadableException.class})
+    private ResponseEntity<ErrorData> integrityViolation(final Throwable exception) {
+        return new ResponseEntity<>(new ErrorData(HttpStatus.BAD_REQUEST.value(), exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorData> handleException(final Throwable throwable) {
